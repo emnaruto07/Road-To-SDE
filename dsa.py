0388664ae20534b4f404fc2a51b6e8d10c0951bd
emnaruto07 <shazebvps@gmail.com>
2022-11-20 00:37:33 +0300

    forloop way

--- dsa.py
+++ dsa.py
@@ -1,7 +1,15 @@
 # DSA reverse string
 
-def reverse(string):
-    string = string[::-1]
-    return string
+# def reverse(string):
+#     string = string[::-1]
+#     return string
 
-print(reverse("shazeb"))
+# print(reverse("shazeb"))
+
+def reverseTwo(string):
+    strr = ""
+    for i in string:
+        strr = i + strr
+    return strr
+
+print(reverseTwo("shazeb"))
