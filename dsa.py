c961b1c794152ad6e5c3ab8d801ea02a0cd97a93
emnaruto07 <shazebvps@gmail.com>
2022-11-20 13:02:42 +0300

    factorial

--- dsa.py
+++ dsa.py
@@ -6,10 +6,28 @@
 
 # print(reverse("shazeb"))
 
-def reverseTwo(string):
-    strr = ""
-    for i in string:
-        strr = i + strr
-    return strr
+# def reverseTwo(string):
+#     strr = ""
+#     for i in string:
+#         strr = i + strr
+#     return strr
 
-print(reverseTwo("shazeb"))
+# print(reverseTwo("shazeb"))
+
+# def rec(n):
+#     if n > 0:
+#         print("Recursive on with", n)
+#         rec(n-1)
+#         rec(n-1)
+
+
+# n=3
+# rec(n)
+
+def rec(n):
+    if n == 1:
+        return n
+    else:
+        return n * rec(n-1)
+n=5
+print(rec(n))
