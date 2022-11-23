dd5b81262c5296e8e15807d1c645372e7ebf5e94
emnaruto07 <shazebvps@gmail.com>
2022-11-23 12:05:17 +0300

    Tower of Hanoi

--- dsa.py
+++ dsa.py
@@ -34,20 +34,34 @@ n=5
 
 # applying rec on fibonacci
 
-def fib(n):
-    #making default case
-    if n <= 1:
-        return n
-    else:
-        #adding last two values
-        return (fib(n-1) + fib(n-2))
+# def fib(n):
+#     #making default case
+#     if n <= 1:
+#         return n
+#     else:
+#         #adding last two values
+#         return (fib(n-1) + fib(n-2))
 
-count = 12
+# count = 12
 
-if n <=0:
-    print("xyz")
-else:
-    sum = 0
-    for i in range(count):
-        sum = sum + fib(i)
-    print(sum)
+# if count <=0:
+#     print("xyz")
+# else:
+#     sum = 0
+#     for i in range(count):
+#         sum = sum + fib(i)
+#     print(sum)
+
+
+#tower of Hanoi
+
+def toh(n, from_bar, to_bar, aux_bar):
+    if n == 1:
+        print("Move disk 1 from bar ", from_bar, " to bar ", to_bar)
+        return
+    toh(n-1, from_bar, aux_bar, to_bar)
+    print("Move disk", n, "from bar ", from_bar, "to bar", to_bar)
+    toh(n-1,aux_bar, to_bar, from_bar)
+
+n = 3
+toh(n, 'A', 'C', 'B')
